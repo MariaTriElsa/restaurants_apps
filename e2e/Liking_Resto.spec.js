4cfc2b4f4167758528209fc67287120a6628837f
MariaTriElsa <mariatrielsa474@gmail.com>
2023-05-23 12:43:53 +0700

    first commit

---
+++ e2e/Liking_Resto.spec.js
@@ -0,0 +1,15 @@
+Feature('Liking Resto');
+ 
+Before(({ I }) => {
+  I.amOnPage('/#/like');
+});
+ 
+Scenario('showing empty liked resto', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada resto untuk ditampilkan', '.resto-item__not__found');
+});
+Scenario('liking one resto', ({ I }) => {
+  I.see('Tidak ada resto untuk ditampilkan', '.resto-item__not__found');
+  I.amOnPage('/');
+  pause();
+});
